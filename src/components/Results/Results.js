64ec26de9995c993c997369101e91e0069e633ca
Apdet <deterding1.a@gmail.com>
2020-10-22 12:44:09 -0500

    completed header component

---
+++ src/components/Results/Results.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Results.css';
+
+const Results = () => {
+    return (
+        <div className="results">
+            
+        </div>
+    );
+}
+
+export default Results;
